0e464d0c98e0848eb2d7c1fbdfe8a7252a1d6ea2
marcelooliveira <mclricardo@gmail.com>
2019-05-08 12:55:44 -0300

    CustomErrorDescriber

--- Item02/CasaDoCodigo/Areas/Identity/CustomErrorDescriber.cs
+++ Item02/CasaDoCodigo/Areas/Identity/CustomErrorDescriber.cs
@@ -2,40 +2,27 @@
 
 namespace CasaDoCodigo.Areas.Identity
 {
-    //public class CustomErrorDescriber : IdentityErrorDescriber
-    //{
-    //    public override IdentityError DefaultError() { return new IdentityError { Code = nameof(DefaultError), Description = $"Ocorreu um erro." }; }
-    //    public override IdentityError ConcurrencyFailure() { return new IdentityError { Code = nameof(ConcurrencyFailure), Description = "Ocorreu um erro, o objeto foi modificado (Optimistic concurrency failure)." }; }
-    //    public override IdentityError PasswordMismatch() { return new IdentityError { Code = nameof(PasswordMismatch), Description = "Senha incorreta." }; }
-    //    public override IdentityError InvalidToken() { return new IdentityError { Code = nameof(InvalidToken), Description = "Você digitou um código inválido." }; }
-    //    public override IdentityError LoginAlreadyAssociated() { return new IdentityError { Code = nameof(LoginAlreadyAssociated), Description = "Já existe um usuário com este nome." }; }
-    //    public override IdentityError InvalidUserName(string userName) { return new IdentityError { Code = nameof(InvalidUserName), Description = $"O nome de usuário '{userName}' é inválido. Ele só pode conter letras e números." }; }
-    //    public override IdentityError InvalidEmail(string email) { return new IdentityError { Code = nameof(InvalidEmail), Description = $"O endereço de e-mail '{email}' está incorreto." }; }
-    //    public override IdentityError DuplicateUserName(string userName) { return new IdentityError { Code = nameof(DuplicateUserName), Description = $"O usuário '{userName}' já existe, por favor digite um nome diferente." }; }
-    //    public override IdentityError DuplicateEmail(string email) { return new IdentityError { Code = nameof(DuplicateEmail), Description = $"O endereço de e-mail '{email}' já está registrado. Recupere sua senha para entrar novamente no sistema." }; }
-    //    public override IdentityError InvalidRoleName(string role) { return new IdentityError { Code = nameof(InvalidRoleName), Description = $"O nome do papel '{role}' é inválido." }; }
-    //    public override IdentityError DuplicateRoleName(string role) { return new IdentityError { Code = nameof(DuplicateRoleName), Description = $"O nome do papel '{role}' já existe." }; }
-    //    public override IdentityError UserAlreadyHasPassword() { return new IdentityError { Code = nameof(UserAlreadyHasPassword), Description = "O usuário já possui senha." }; }
-    //    public override IdentityError UserLockoutNotEnabled() { return new IdentityError { Code = nameof(UserLockoutNotEnabled), Description = "O bloqueio não está habilitado para este usuário." }; }
-    //    public override IdentityError UserAlreadyInRole(string role) { return new IdentityError { Code = nameof(UserAlreadyInRole), Description = $"O usuário já faz parte do papel '{role}'." }; }
-    //    public override IdentityError UserNotInRole(string role) { return new IdentityError { Code = nameof(UserNotInRole), Description = $"O usuário não faz parte do papel '{role}'." }; }
-    //    public override IdentityError PasswordTooShort(int length) { return new IdentityError { Code = nameof(PasswordTooShort), Description = $"A senha deve ter um número mínimo de {length} caracteres." }; }
-    //    public override IdentityError PasswordRequiresNonAlphanumeric() { return new IdentityError { Code = nameof(PasswordRequiresNonAlphanumeric), Description = "A senha deve conter pelo menos um caracter alfanumérico." }; }
-    //    public override IdentityError PasswordRequiresDigit() { return new IdentityError { Code = nameof(PasswordRequiresDigit), Description = "A senha deve conter pelo menos um dígito ('0'-'9')." }; }
-    //    public override IdentityError PasswordRequiresLower() { return new IdentityError { Code = nameof(PasswordRequiresLower), Description = "A senha deve conter pelo menos uma letra minúscula ('a'-'z')." }; }
-    //    public override IdentityError PasswordRequiresUpper() { return new IdentityError { Code = nameof(PasswordRequiresUpper), Description = "A senha deve conter pelo menos uma letra maiúscula ('A'-'Z')." }; }
-    //}
-
     public class CustomErrorDescriber : IdentityErrorDescriber
     {
-        public override IdentityError PasswordRequiresNonAlphanumeric()
-        { return new IdentityError { Code = nameof(PasswordRequiresNonAlphanumeric),
-            Description = "A senha deve conter pelo menos um caracter alfanumérico." }; }
-        public override IdentityError PasswordRequiresLower()
-        { return new IdentityError { Code = nameof(PasswordRequiresLower),
-            Description = "A senha deve conter pelo menos uma letra minúscula ('a'-'z')." }; }
-        public override IdentityError PasswordRequiresUpper()
-        { return new IdentityError { Code = nameof(PasswordRequiresUpper),
-            Description = "A senha deve conter pelo menos uma letra maiúscula ('A'-'Z')." }; }
+        public override IdentityError DefaultError() { return new IdentityError { Code = nameof(DefaultError), Description = $"Ocorreu um erro." }; }
+        public override IdentityError ConcurrencyFailure() { return new IdentityError { Code = nameof(ConcurrencyFailure), Description = "Ocorreu um erro, o objeto foi modificado (Optimistic concurrency failure)." }; }
+        public override IdentityError PasswordMismatch() { return new IdentityError { Code = nameof(PasswordMismatch), Description = "Senha incorreta." }; }
+        public override IdentityError InvalidToken() { return new IdentityError { Code = nameof(InvalidToken), Description = "Você digitou um código inválido." }; }
+        public override IdentityError LoginAlreadyAssociated() { return new IdentityError { Code = nameof(LoginAlreadyAssociated), Description = "Já existe um usuário com este nome." }; }
+        public override IdentityError InvalidUserName(string userName) { return new IdentityError { Code = nameof(InvalidUserName), Description = $"O nome de usuário '{userName}' é inválido. Ele só pode conter letras e números." }; }
+        public override IdentityError InvalidEmail(string email) { return new IdentityError { Code = nameof(InvalidEmail), Description = $"O endereço de e-mail '{email}' está incorreto." }; }
+        public override IdentityError DuplicateUserName(string userName) { return new IdentityError { Code = nameof(DuplicateUserName), Description = $"O usuário '{userName}' já existe, por favor digite um nome diferente." }; }
+        public override IdentityError DuplicateEmail(string email) { return new IdentityError { Code = nameof(DuplicateEmail), Description = $"O endereço de e-mail '{email}' já está registrado. Recupere sua senha para entrar novamente no sistema." }; }
+        public override IdentityError InvalidRoleName(string role) { return new IdentityError { Code = nameof(InvalidRoleName), Description = $"O nome do papel '{role}' é inválido." }; }
+        public override IdentityError DuplicateRoleName(string role) { return new IdentityError { Code = nameof(DuplicateRoleName), Description = $"O nome do papel '{role}' já existe." }; }
+        public override IdentityError UserAlreadyHasPassword() { return new IdentityError { Code = nameof(UserAlreadyHasPassword), Description = "O usuário já possui senha." }; }
+        public override IdentityError UserLockoutNotEnabled() { return new IdentityError { Code = nameof(UserLockoutNotEnabled), Description = "O bloqueio não está habilitado para este usuário." }; }
+        public override IdentityError UserAlreadyInRole(string role) { return new IdentityError { Code = nameof(UserAlreadyInRole), Description = $"O usuário já faz parte do papel '{role}'." }; }
+        public override IdentityError UserNotInRole(string role) { return new IdentityError { Code = nameof(UserNotInRole), Description = $"O usuário não faz parte do papel '{role}'." }; }
+        public override IdentityError PasswordTooShort(int length) { return new IdentityError { Code = nameof(PasswordTooShort), Description = $"A senha deve ter um número mínimo de {length} caracteres." }; }
+        public override IdentityError PasswordRequiresNonAlphanumeric() { return new IdentityError { Code = nameof(PasswordRequiresNonAlphanumeric), Description = "A senha deve conter pelo menos um caracter alfanumérico." }; }
+        public override IdentityError PasswordRequiresDigit() { return new IdentityError { Code = nameof(PasswordRequiresDigit), Description = "A senha deve conter pelo menos um dígito ('0'-'9')." }; }
+        public override IdentityError PasswordRequiresLower() { return new IdentityError { Code = nameof(PasswordRequiresLower), Description = "A senha deve conter pelo menos uma letra minúscula ('a'-'z')." }; }
+        public override IdentityError PasswordRequiresUpper() { return new IdentityError { Code = nameof(PasswordRequiresUpper), Description = "A senha deve conter pelo menos uma letra maiúscula ('A'-'Z')." }; }
     }
 }
