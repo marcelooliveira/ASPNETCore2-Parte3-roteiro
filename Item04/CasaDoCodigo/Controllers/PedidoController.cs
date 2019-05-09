e52c551f1608e5814f61204f1533da9d0cebcc66
marcelooliveira <mclricardo@gmail.com>
2019-05-09 13:48:49 -0300

    Migração Pedido_ClienteId

---
+++ Item04/CasaDoCodigo/Controllers/PedidoController.cs
@@ -0,0 +1,98 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Models.ViewModels;
+using CasaDoCodigo.Repositories;
+using IdentityModel;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly IProdutoRepository produtoRepository;
+        private readonly IPedidoRepository pedidoRepository;
+
+        public PedidoController(IProdutoRepository produtoRepository,
+            IPedidoRepository pedidoRepository)
+        {
+            this.produtoRepository = produtoRepository;
+            this.pedidoRepository = pedidoRepository;
+        }
+
+        public async Task<IActionResult> Carrossel()
+        {
+            return View(await produtoRepository.GetProdutosAsync());
+        }
+
+        public async Task<IActionResult> BuscaProdutos(string pesquisa)
+        {
+            return View(await produtoRepository.GetProdutosAsync(pesquisa));
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Carrinho(string codigo)
+        {
+            string userId = GetUserId();
+
+            if (!string.IsNullOrEmpty(codigo))
+            {
+                await pedidoRepository.AddItemAsync(codigo);
+            }
+
+            var pedido = await pedidoRepository.GetPedidoAsync();
+            List<ItemPedido> itens = pedido.Itens;
+            CarrinhoViewModel carrinhoViewModel = new CarrinhoViewModel(itens);
+            return base.View(carrinhoViewModel);
+        }
+
+        [Authorize]
+        public async Task<IActionResult> Cadastro()
+        {
+            var pedido = await pedidoRepository.GetPedidoAsync();
+
+            if (pedido == null)
+            {
+                return RedirectToAction("Carrossel");
+            }
+
+            return View(pedido.Cadastro);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<IActionResult> Resumo(Cadastro cadastro)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(await pedidoRepository.UpdateCadastroAsync(cadastro));
+            }
+            return RedirectToAction("Cadastro");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<UpdateQuantidadeResponse> UpdateQuantidade([FromBody]ItemPedido itemPedido)
+        {
+            return await pedidoRepository.UpdateQuantidadeAsync(itemPedido);
+        }
+
+        private string GetUserId()
+        {
+            var claims = User.Claims.ToList();
+            foreach (var claim in claims)
+            {
+                Debug.WriteLine($"claim: {claim.Type}, valor: {claim.Value}");
+            }
+            return User.FindFirst(JwtClaimTypes.Subject)?.Value;
+        }
+
+    }
+}
