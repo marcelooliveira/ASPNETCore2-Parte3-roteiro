c6105de6f3727191b2ddfc295b10f0aa04600692
marcelooliveira <mclricardo@gmail.com>
2019-05-13 17:49:35 -0300

    .

--- Item04/CasaDoCodigo/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Item04/CasaDoCodigo/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -45,7 +45,7 @@ namespace CasaDoCodigo.Areas.Identity.Pages.Account
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Lembrar de mim?")]
             public bool RememberMe { get; set; }
         }
 
